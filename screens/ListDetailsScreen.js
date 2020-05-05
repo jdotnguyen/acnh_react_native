f0a438c95fc64780e6ad9ba39e9e7ea2c228cbad
Justin Nguyen <justin_nguyen@outlook.com>
2020-05-05 18:58:16 -0400

    Stylize fish details component

--- screens/ListDetailsScreen.js
+++ screens/ListDetailsScreen.js
@@ -34,14 +34,42 @@ export default class ListDetailsScreen extends Component {
             <SafeAreaView>
                 <ScrollView style={getContentBodyStyles(this.background)}>
                     <View style={styles.innerContent}>
+                        {/* Name and icon */}
                         <View style={styles.headerContainer}>
                             <Text style={styles.header}>{this.data.name['name-en']}</Text>
                             <Image style={styles.image} source={{ uri: this.data.icon }} />
+
+                            {/* Catchphrase */}
+                            <Text style={styles.detailsInfoHeader}>Catchphrase:</Text>
+                            <Text style={styles.detailsInfo}>"{this.data['catch-phrase']}"</Text>
+
+                            {/* Location */}
+                            <Text style={styles.detailsInfoHeader}>Location:</Text>
+                            <Text style={styles.detailsInfo}>{this.data['availability']['location']}</Text>
                         </View>
+
+                        {/* Information */}
                         <View style={styles.detailsInfoContainer}>
+                            {/* Prices */}
                             <Text style={styles.detailsInfoHeader}>Prices:</Text>
-                            <Text style={getDetailsInfo(AmericanPalette.DARK_GREEN)}>{this.data.price}</Text>
-                            <Text style={getDetailsInfo(AmericanPalette.DARK_RED)}>{this.data['price-cj']} (CJ)</Text>
+                            <View style={getDetailsInfo(AmericanPalette.DARK_GREEN)}><Text style={styles.detailsInfoWhite}>{this.data.price}</Text></View>
+                            <View style={getDetailsInfo(AmericanPalette.DARK_RED, true)}><Text style={styles.detailsInfoWhite}>{this.data['price-cj']} (CJ)</Text></View>
+
+                            {/* Availability months */}
+                            <Text style={styles.detailsInfoHeader}>Months:</Text>
+                            <Text style={styles.detailsInfo}>{this.data['availability']['month-northern'] ? this.data['availability']['month-northern'] : 'All'}</Text>
+
+                            {/* Availability all-day */}
+                            <Text style={styles.detailsInfoHeader}>Time:</Text>
+                            <Text style={styles.detailsInfo}>{this.data['availability']['isAllDay'] ? 'All-day' : this.data['availability']['time']}</Text>
+
+                            {/* Shadow Size */}
+                            <Text style={styles.detailsInfoHeader}>Shadow Size:</Text>
+                            <Text style={styles.detailsInfo}>{this.data['shadow']}</Text>
+
+                            {/* Rarity */}
+                            <Text style={styles.detailsInfoHeader}>Rarity:</Text>
+                            <Text style={styles.detailsInfo}>{this.data['availability']['rarity']}</Text>
                         </View>
                     </View>
                 </ScrollView>
@@ -55,10 +83,13 @@ export default class ListDetailsScreen extends Component {
             <SafeAreaView>
                 <ScrollView style={getContentBodyStyles(this.background)}>
                     <View style={styles.innerContent}>
+                        {/* Name and icon */}
                         <View style={styles.headerContainer}>
                             <Text style={styles.header}>{this.data.name['name-en']}</Text>
                             <Image style={styles.image} source={{ uri: this.data.icon }} />
                         </View>
+
+                        {/* Information */}
                         <View style={styles.detailsInfoContainer}>
                             <Text style={styles.detailsInfoHeader}>Prices:</Text>
                             <Text style={styles.detailsInfo}>{this.data.price} Bells</Text>
@@ -80,13 +111,17 @@ export default class ListDetailsScreen extends Component {
                         <View style={styles.headerContainer}>
                             <Text style={styles.header}>{this.data.name['name-en']}</Text>
                             <Image style={styles.image} source={{ uri: this.data.icon }} />
+
+                            {/* Catchphrase */}
+                            <Text style={styles.detailsInfoHeader}>Catchphrase:</Text>
+                            <Text style={styles.detailsInfo}>"{this.data['catch-phrase']}"</Text>
                         </View>
 
                         {/* Information */}
                         <View style={styles.detailsInfoContainer}>
                             {/* Personality */}
                             <Text style={styles.detailsInfoHeader}>Personality:</Text>
-                            <View style={getDetailsInfo(VillagerMoods[this.data.personality])}><Text style={styles.detailsInfoWhite}>{this.data.personality}</Text></View>
+                            <View style={getDetailsInfo(VillagerMoods[this.data.personality], true)}><Text style={styles.detailsInfoWhite}>{this.data.personality}</Text></View>
 
                             {/* Birthday */}
                             <Text style={styles.detailsInfoHeader}>Birthday:</Text>
@@ -99,10 +134,6 @@ export default class ListDetailsScreen extends Component {
                             {/* Gender */}
                             <Text style={styles.detailsInfoHeader}>Gender:</Text>
                             <Text style={styles.detailsInfo}>{this.data.gender}</Text>
-
-                            {/* Catchphrase */}
-                            <Text style={styles.detailsInfoHeader}>Catchphrase:</Text>
-                            <Text style={styles.detailsInfo}>"{this.data['catch-phrase']}"</Text>
                         </View>
                     </View>
                 </ScrollView>
@@ -130,14 +161,14 @@ const getContentBodyStyles = (background) => {
     }
 }
 
-const getDetailsInfo = (background) => {
+const getDetailsInfo = (background, indent) => {
     return {
         alignItems: 'center',
         padding: 5,
         paddingLeft: 10,
         paddingRight: 10,
         backgroundColor: background,
-        marginBottom: 30,
+        marginBottom: indent ? 30 : 5, // Optional param to add space below
         borderRadius: 20
     }
 }
@@ -155,7 +186,6 @@ const styles = StyleSheet.create({
         flexDirection: 'row'
     },
     headerContainer: {
-        alignItems: 'center',
         marginHorizontal: 20,
         width: '40%'
     },
@@ -169,7 +199,8 @@ const styles = StyleSheet.create({
     },
     detailsInfoContainer: {
         marginBottom: 10,
-        marginLeft: 20
+        marginLeft: 20,
+        width: '35%'
     },
     detailsInfoHeader: {
         fontSize: 18,
